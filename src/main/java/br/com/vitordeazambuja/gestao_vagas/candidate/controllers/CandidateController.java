cf2d9734dc1e811844729bfb36f1ea1e165f9b39
Vitor <vitordeazambuja@edu.uniube.br>
2026-01-19 13:59:15 -0300

    feat: criado controller de candidato

---
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/controllers/CandidateController.java
@@ -0,0 +1,17 @@
+package br.com.vitordeazambuja.gestao_vagas.candidate.controllers;
+
+import br.com.vitordeazambuja.gestao_vagas.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity){
+        System.out.println(candidateEntity.getEmail());
+    }
+}
