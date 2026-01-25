a28dd4ecf056e96f97d77cb4618521f6fead6dc2
Vitor <vitordeazambuja@gmail.com>
2026-01-25 20:01:43 -0300

    feat: adicionada criptografia da senha do candidato

--- src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.vitordeazambuja.gestao_vagas.candidate.entities.CandidateEntity;
 import br.com.vitordeazambuja.gestao_vagas.candidate.repositories.CandidateRepository;
 import br.com.vitordeazambuja.gestao_vagas.exceptions.UserFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity){
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user)->{
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
