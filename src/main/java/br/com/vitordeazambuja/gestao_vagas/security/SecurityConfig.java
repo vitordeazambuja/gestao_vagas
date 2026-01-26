101a25f2cb0bf7967972646a098d4d310746eaff
Vitor <vitordeazambuja@edu.uniube.br>
2026-01-26 13:15:10 -0300

    feat: criada a primeira parte do filtro de segurança do candidato

--- src/main/java/br/com/vitordeazambuja/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/security/SecurityConfig.java
@@ -15,6 +15,9 @@ public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
 
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -26,6 +29,7 @@ public class SecurityConfig {
                     auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
 
         ;
         return http.build();
